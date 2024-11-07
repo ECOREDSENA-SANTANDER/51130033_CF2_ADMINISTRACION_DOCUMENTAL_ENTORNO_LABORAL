b05525409cd4a0179a7e2063f507e76ba3272592
Andrea Botello <andrea.botellor@misena.edu.co>
2024-11-07 12:17:38 -0500

    Avance2

--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -7,9 +7,78 @@
         span 4
       h1 Principios y normas archivísticas
     
+    .bloque-texto-g.bg2.p-3.p-sm-4.p-md-5.mb-5
+      .bloque-texto-g__img(
+        :style="{'background-image': `url(${require('@/assets/curso/temas/t4/img1.svg')})`}"
+      )
+      .bloque-texto-g__texto.p-4
+        p.mb-0 Los archivos y centros documentales han venido aumentando su participación, causado por la gran necesidad de información requerida por los diversos usuarios; esto ha traído como consecuencia la definición de requisitos y procedimientos que estructuren el análisis documental, tomando como referente organizacional las especiales características de cada documento y la instauración de un adecuado método de archivo, dando origen a las normas y principios de archivística como instrumento regulador que garantice el intercambio de información, y facilite el régimen requerido para la administración documental; aplicando, además, procedimientos técnicos en la recepción y despacho documental. 
     
+    Separador
+    #t_4_1.titulo-segundo.color-acento-contenido(data-aos="flip-up")
+      h2 4.1 Pautas para la administración de comunicaciones oficiales
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-10
+        .cajon.color-secundario.p-4.mb-4
+          p #[b ACUERDO No. 001 del 2024 (29 de febrero) “Por el cual se establece el Acuerdo Único de la Función Archivística, se definen los criterios técnicos y jurídicos para su implementación en el Estado Colombiano y se fijan otras disposiciones.”:] norma establecida para orientar la adecuada prestación de los servicios ofrecidos por las unidades de correspondencia de las entidades públicas y privadas, o aquellas privadas que cumplen funciones públicas, instituyendo pautas que hagan efectivo su cumplimiento bajo los principios que rigen la administración pública. 
     
+    .row.mb-4
+      .col-lg-8
+        p.mb-3 #[b Norma Icontec GTC 185:] el propósito de esta norma es el de brindar herramientas a las empresas para que le faciliten la gestión de los documentos. Busca contribuir con la disminución del tiempo y costos sin dejar de lado la calidad. Esta norma aplica tanto a las entidades gubernamentales como al sector privado, con el fin de orientar sus procesos archivísticos hacia una gestión de mejoramiento continuo, haciéndolo cada vez más competitivo. 
+        p El primordial objeto de la Norma Icontec GTC 185, es el de plantear una serie de recomendaciones para la elaboración de los diversos tipos de documentos que generan las organizaciones. Teniendo en cuenta que, su enfoque es el de facilitar la proyección de una apropiada imagen corporativa, al establecer una vinculación directa entre la norma prescrita y todo el personal involucrado en la generación de documentos institucionales. 
+      .col-lg-4
+        img(src="@/assets/curso/temas/t4/img2.png", alt="")
     
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-10
+        .titulo-sexto.color-acento-contenido
+          h5 Figura 1.
+          span Normas de gestión de calidad.
+        img(src="@/assets/curso/temas/t4/fig1.svg", alt=" La figura menciona las siguientes normas de gestión de calidad: -NTC1001 papel, formatos. -NTC 1486 presentación de tesis y trabajos de grado. -NTC 1057 Documentación. Guía para numeración de divisiones y subdivisiones en documentos escritos.-NTC 4436 información y documentación. Papel para documentos de archivo. Requisitos para la permanencia y durabilidad. -NTC5313 Información y documentación Reglas para la abreviatura de títulos de publicaciones.")
+        figcaption Fuente: SENA.
+    
+    p.mb-4 La norma hace especificaciones orientadoras sobre los diversos documentos, en aspectos relacionados con su estructura y aplicabilidad. Algunos documentos referenciados dentro de esta normatividad son los siguientes: 
+
+    .row.mb-4
+      .col-lg-3
+        img(src="@/assets/curso/temas/t4/img3.png", alt="")
+      .col-lg-9
+        SlyderF.mb-5(columnas="col-lg-9 col-xl-4")
+          .tarjeta.color-secundario.p-4
+            .row.justify-content-center.mb-3
+              .col-8
+                img(src='@/assets/curso/temas/t4/slyder1.svg' alt='AvatarTop')
+            h2.text-center  Carta comercial
+            p.text-center Comunicación escrita que se utiliza en las relaciones comerciales entre entidades y personas naturales; igualmente en las relaciones entre las empresas y sus empleados.
+          
+          .tarjeta.color-secundario.p-4
+            .row.justify-content-center.mb-3
+              .col-8
+                img(src='@/assets/curso/temas/t4/slyder2.svg' alt='AvatarTop')
+            h2.text-center  Circular
+            p.text-center Comunicación escrita de interés común, con el mismo contenido dirigido a un grupo específico. 
+          
+          .tarjeta.color-secundario.p-4
+            .row.justify-content-center.mb-3
+              .col-8
+                img(src='@/assets/curso/temas/t4/slyder3.svg' alt='AvatarTop')
+            h2.text-center  Memorando
+            p.text-center Comunicación escrita que se emplea en las empresas para tratar asuntos internos. 
+
+          .tarjeta.color-secundario.p-4
+            .row.justify-content-center.mb-3
+              .col-8
+                img(src='@/assets/curso/temas/t4/slyder4.svg' alt='AvatarTop')
+            h2.text-center  Actas administrativas
+            p.text-center Documento escrito que tiene lo sucedido, tratado o acordado en una reunión o situación específica.
+          
+          .tarjeta.color-secundario.p-4
+            .row.justify-content-center.mb-3
+              .col-8
+                img(src='@/assets/curso/temas/t4/slyder5.svg' alt='AvatarTop')
+            h2.text-center   Informe administrativo
+            p.text-center Documento que describe o da a conocer datos precisos sobre el estado de cualquier actividad, relativos a un caso concreto para conocer resultados de procesos administrativos.
 
 </template>
 
--- src/views/Tema6.vue
+++ src/views/Tema6.vue
@@ -7,7 +7,24 @@
         span 6
       h1 Embalaje de los documentos
     
-    
+    .row
+      .col-lg-3
+        img(src="@/assets/curso/temas/t6/img1.png", alt="")
+      .col-lg-9.bgc-2
+        .tarjeta.tarjeta--blanca.p-4.mb-5
+          SlyderA
+            .row
+              .col-md-4.mb-4.mb-md-0
+                img(src="@/assets/curso/temas/t6/slyder1.svg", alt="")            
+              .col-md-8
+                h5 El empaque
+                p Suele ser utilizado generalmente en las estrategias de marketing para garantizar la calidad de un producto. Pero cuando se habla de la gestión documental se entiende por empaque al sobre, envoltura, plastificación o cubierta en otro tipo de material, donde se guarda el documento al momento de su despacho para protegerlo hasta el momento de la entrega al destinatario.
+            .row
+              h5 El embalaje
+              p Generalmente suele asociarse para cantidades más grandes de documentos o acervos que requieren ser trasladados, buscando conservar sus propiedades antes de iniciar el recorrido, durante el mismo y al final de su entrega. Se diligencia un formato con los datos más relevantes del documento, generalmente se traza un diseño específico según la empresa y los criterios documentales que se tengan en ella. Cuando la correspondencia está orientada a dar una respuesta, no sólo se registran los criterios de su salida o despacho sino que se anota también el consecutivo y fecha del documento que antecede a este; esta operación constituye el control y el trámite documental.
+            .row
+              h5 El envío
+              p En primera medida se necesita tener claridad sobre la forma de envío que se desea realizar, definiendo el nivel geográfico de su traslado y las condiciones de empaque y seguridad; estos procedimientos deben garantizar que el documento llegue a su destino final sin sufrir alteraciones en su contenido o esencia. Además, se debe escoger la mejor forma de empaque, la empresa que prestará el servicio y la verificación de tarifas que implica realizar esta gestión; ya que, se debe ser consciente que acudir a este medio, requiere tener un fondo que solvente los costos de la unidad de correspondencia.
     
     
 
